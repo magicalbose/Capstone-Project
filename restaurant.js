46cdc37340ce1c90f0b37455ca032d1c973da4b1
magicalbose <peehana.bose@gmail.com>
2025-07-17 23:23:45 -0500

    Add files via upload

---
+++ restaurant.js
@@ -0,0 +1,9 @@
+function Food() {
+    window.location.href = "about1.html";
+}
+function done() {
+    window.location.href = "delivery.html";
+}
+function back() {
+    window.location.href = "restaurant.html";
+}
