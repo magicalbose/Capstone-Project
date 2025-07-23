559a0aba33a678e71d151727f578eec9d6860fe6
magicalbose <peehana.bose@gmail.com>
2025-07-23 16:32:11 -0500

    Delete restaurant.js

--- restaurant.js
+++
@@ -1,9 +0,0 @@
-function Food() {
-    window.location.href = "about1.html";
-}
-function done() {
-    window.location.href = "delivery.html";
-}
-function back() {
-    window.location.href = "restaurant.html";
-}
